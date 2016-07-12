0b924d5abf9a40f560523aa12ba65c1c201a6f16
thln <tamhenrylenguyen@gmail.com>
2016-07-11 21:32:01 -0700

    Testing Git Commit

--- Shuttermail/TLNCameraViewController.m
+++ Shuttermail/TLNCameraViewController.m
@@ -19,6 +19,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //Testing update on Git
     
     self.model = [TLNShuttermailModel sharedModel];
     
